4bd98311e0886087a466bcd9b243be3f4a6a03bb
ben foley <ben@cbmm.io>
2021-12-25 15:17:02 +1000

    wip refactor

--- test-output/_assets/script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-  $(".entryAudio" ).click(function() {
-    var url = $(this).data("file");
-    $( "#wsumarcs-dummycat" ).html('<embed src=\"' + url + '\" hidden=\"true\" autostart=\"true\" loop=\"false\" />');
-  });
-});
