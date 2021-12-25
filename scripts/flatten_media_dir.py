bb5632dbff16b58217607363f12c78fbd2d10834
ben foley <ben@cbmm.io>
2021-12-25 19:16:00 +1000

    enable processing all langs

--- scripts/flatten_media_dir.py
+++ scripts/flatten_media_dir.py
@@ -1,24 +1,28 @@
 import shutil
 from pathlib import Path
 
-language = "test"
-# language = "bilinarra"
-# language = "gurindji"
-# language = "mudburra"
-# language = "ngarinyman"
+# languages = ["test"]
 
-source_path = Path(f"../content/{language}")
-target_path = Path(f"../output/{language}/")
+languages = [
+    "bilinarra",
+    "gurindji",
+    "mudburra",
+    "ngarinyman"]
 
-target_path.joinpath("_audio").mkdir(parents=True, exist_ok=True)
-target_path.joinpath("_img").mkdir(parents=True, exist_ok=True)
+for language in languages:
 
-# Process audio
-for each_file in source_path.glob('**/*.mp3'):
-    print(each_file)
-    shutil.copy(each_file, target_path.joinpath("_audio"))
+    source_path = Path(f"../content/{language}")
+    target_path = Path(f"../output/{language}/")
 
-# Process images
-for each_file in source_path.glob('**/*.jpg'):
-    print(each_file)
-    shutil.copy(each_file, target_path.joinpath("_img"))
+    target_path.joinpath("_audio").mkdir(parents=True, exist_ok=True)
+    target_path.joinpath("_img").mkdir(parents=True, exist_ok=True)
+
+    # Process audio
+    for each_file in source_path.glob('**/*.mp3'):
+        print(each_file)
+        shutil.copy(each_file, target_path.joinpath("_audio"))
+
+    # Process images
+    for each_file in source_path.glob('**/*.jpg'):
+        print(each_file)
+        shutil.copy(each_file, target_path.joinpath("_img"))
